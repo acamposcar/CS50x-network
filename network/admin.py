aa88324778e57d88e24d0507708b46147b9c6a35
Alejandro Campos <acamposcar@gmail.com>
2022-03-07 20:00:28 +0100

    Create Models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Post, Likes, Followers, Comment
 
 # Register your models here.
+""" class Post(admin.ModelAdmin):
+    list_display = ("__str__", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+     """
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Likes)
+admin.site.register(Followers)
+admin.site.register(Comment)
+
--- network/models.py
+++ network/models.py
@@ -1,6 +1,77 @@
+from lib2to3.pgen2.pgen import DFAState
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    profile_image = models.URLField(default="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/240px-OOjs_UI_icon_userAvatar.svg.png")
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    image = models.URLField(default='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/240px-OOjs_UI_icon_userAvatar.svg.png')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "image": self.image
+        }
+
+    def __str__(self):
+        return f"Post {self.id}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_comments")
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="post_comments", blank=True)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "post": self.post.id,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
+
+    def __str__(self):
+        return f"Comment {self.content}"
+
+
+class Likes(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_likes")
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="posts_likes", blank=True, null=True)
+    comment = models.ForeignKey("Comment", on_delete=models.CASCADE, related_name="comment_likes", blank=True, null=True) 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "post": self.post.id,
+            "comment": self.comment.content
+        }
+
+    def __str__(self):
+        return f"{self.user} likes {self.post} or {self.comment}"
+
+
+class Followers(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_followers")
+    following = models.ManyToManyField("User", related_name="user_following", blank=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "following": [user.username for user in self.following.all()],
+        }
+
+    def __str__(self):
+        return f"Followers {self.user}"
