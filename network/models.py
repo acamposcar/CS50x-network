7afa9bc24dde21e7573983aed9951213d9835859
Alejandro Campos <acamposcar@gmail.com>
2022-03-08 20:29:31 +0100

    Show likes and comments in post

--- network/models.py
+++ network/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_comments")
-    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="post_comments", blank=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments", blank=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
