82aed5943ff63c211fe902474a1f8cec2c30bfec
Alejandro Campos <acamposcar@gmail.com>
2022-03-16 17:05:46 +0100

    Add arrow svg to ident comments

--- network/static/network/utils.js
+++ network/static/network/utils.js
@@ -1,20 +1,27 @@
 export function createCommentContainer(comment, post) {
   const container = post.querySelector('.container-comments');
   container.innerHTML += `
+
+                      
                       <div class="comment" id="comment_${comment.id}">
-                        <div class="post-header d-flex align-items-center gap-2 user-avatar">
-                           <img class="comment-image" src="${ comment.profile_image }" alt="Profile Image">
-                           <div>
-                              <div class="d-flex align-items-baseline gap-1">
-                                <a class="post-name" href="/users/${comment.username}">${comment.first_name} ${ comment.last_name }</a>
-                                <a class="post-username" href="/users/${comment.username}"><small>@${ comment.username }</small></a>
-                                
-                              </div>
-                              <span class="post-date"><small>${ comment.timestamp }</small></span>
-                            </div>  
-                         </div>  
-                        <div class="comment-content">
-                          ${comment.content.replace(/(\r\n|\n|\r)/g, '<br />')}
+                        <div class="d-flex gap-2">
+                          <img class="mt-3" width="14" height="14" src='/static/network/icons/corner-down-right.svg' alt="Delete Post">
+                          <div>
+                          <div class="post-header d-flex align-items-center gap-2 user-avatar">
+                            <img class="comment-image" src="${ comment.profile_image }" alt="Profile Image">
+                            <div>
+                                <div class="d-flex align-items-baseline gap-1">
+                                  <a class="post-name" href="/users/${comment.username}">${comment.first_name} ${ comment.last_name }</a>
+                                  <a class="post-username" href="/users/${comment.username}"><small>@${ comment.username }</small></a>
+                                  
+                                </div>
+                                <span class="post-date"><small>${ comment.timestamp }</small></span>
+                              </div>  
+                          </div>  
+                          <div class="comment-content">
+                            ${comment.content.replace(/(\r\n|\n|\r)/g, '<br />')}
+                          </div>
+                        </div>
                         </div>
                       </div>
     `;
